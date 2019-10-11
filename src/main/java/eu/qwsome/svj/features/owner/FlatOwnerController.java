c6c7273fa628efdfd8e729865955877a4d2f128d
Lukáš Kvídera <kvideral@qwsome.eu>
2019-10-11 09:50:24 +0200

    Reduced copy paste

--- src/main/java/eu/qwsome/svj/features/owner/FlatOwnerController.java
+++ src/main/java/eu/qwsome/svj/features/owner/FlatOwnerController.java
@@ -6,6 +6,7 @@ import eu.qwsome.svj.shared.view.View;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
@@ -41,15 +42,20 @@ public class FlatOwnerController {
     this.sceneManager = sceneManager;
   }
 
+
+  private static FlatOwner getSelectedFlatOwner(final CellEditEvent<FlatOwner, String> event) {
+    return event.getTableView()
+      .getItems()
+      .get(event.getTablePosition().getRow());
+  }
+
   @FXML
   public void initialize() {
     this.flatOwnerNameTableColumn.setCellFactory(TextFieldTableCell.forTableColumn());
     this.flatOwnerNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
     this.flatOwnerNameTableColumn.setOnEditCommit(
       event -> {
-        final FlatOwner flatOwner = event.getTableView()
-          .getItems()
-          .get(event.getTablePosition().getRow());
+        final FlatOwner flatOwner = getSelectedFlatOwner(event);
         flatOwner.setFirstName(event.getNewValue());
         this.flatOwnerService.save(flatOwner);
       }
@@ -59,9 +65,7 @@ public class FlatOwnerController {
     this.flatOwnerLastNameTableColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
     this.flatOwnerLastNameTableColumn.setOnEditCommit(
       event -> {
-        final FlatOwner flatOwner = event.getTableView()
-          .getItems()
-          .get(event.getTablePosition().getRow());
+        final FlatOwner flatOwner = getSelectedFlatOwner(event);
         flatOwner.setLastName(event.getNewValue());
         this.flatOwnerService.save(flatOwner);
       }
@@ -71,9 +75,7 @@ public class FlatOwnerController {
     this.flatOwnerAnotherNamesTableColumn.setCellValueFactory(new PropertyValueFactory<>("anotherNames"));
     this.flatOwnerAnotherNamesTableColumn.setOnEditCommit(
       event -> {
-        final FlatOwner flatOwner = event.getTableView()
-          .getItems()
-          .get(event.getTablePosition().getRow());
+        final FlatOwner flatOwner = getSelectedFlatOwner(event);
         flatOwner.setAnotherNames(event.getNewValue());
         this.flatOwnerService.save(flatOwner);
       }
@@ -83,21 +85,17 @@ public class FlatOwnerController {
     this.flatOwnerPhoneTableColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
     this.flatOwnerPhoneTableColumn.setOnEditCommit(
       event -> {
-        final FlatOwner flatOwner = event.getTableView()
-          .getItems()
-          .get(event.getTablePosition().getRow());
+        final FlatOwner flatOwner = getSelectedFlatOwner(event);
         flatOwner.setPhone(event.getNewValue());
         this.flatOwnerService.save(flatOwner);
       }
     );
 
     this.flatOwnerEmailTableColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-    this.flatOwnerEmailTableColumn.setCellValueFactory(new PropertyValueFactory<>("phone"));
+    this.flatOwnerEmailTableColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
     this.flatOwnerEmailTableColumn.setOnEditCommit(
       event -> {
-        final FlatOwner flatOwner = event.getTableView()
-          .getItems()
-          .get(event.getTablePosition().getRow());
+        final FlatOwner flatOwner = getSelectedFlatOwner(event);
         flatOwner.setEmail(event.getNewValue());
         this.flatOwnerService.save(flatOwner);
       }
